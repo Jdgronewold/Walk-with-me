f89ec9ec216606b3efdf41d7b2056216f28b7c5e
Eileen Ho <eileenho25@gmail.com>
2017-01-30 10:08:55 -0800

    add sessions and application controller methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,9 @@
-class UsersController < ApplicationController
+class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in(@user)
+      login(@user)
       #render
     else
       render json: @user.errors.full_messages, status: 422
@@ -15,5 +15,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :password)
   end
-  
+
 end
