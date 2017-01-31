146bb7ac4375ed962318abaa97291fb679646b2f
Zach-Haddad <Zhaddad7@gmail.com>
2017-01-31 06:45:54 -0800

    fixed syntax in index.ios.js

--- WalkWithMe/index.ios.js
+++ WalkWithMe/index.ios.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
   apiKey: "<your-api-key>",
   authDomain: "<your-auth-domain>",
   databaseURL: "<your-database-url>",
-  storageBucket: "<your-storage-bucket>",,
+  storageBucket: "<your-storage-bucket>",
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
