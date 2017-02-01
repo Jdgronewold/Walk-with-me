9f6c99e7b398415dcccf96becf5c71e5f54319cc
Jeff Gronewold <jeff.gronewold@gmail.com>
2017-01-31 20:45:38 -0800

    Complete steps to install native-react-maps

--- WalkWithMe/ios/WalkWithMe/AppDelegate.m
+++ WalkWithMe/ios/WalkWithMe/AppDelegate.m
@@ -12,6 +12,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import GoogleMaps;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
