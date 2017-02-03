3ca9c0714b7a99acb1c7b60dbf52e80e34153a36
Eileen Ho <eileenho25@gmail.com>
2017-02-03 12:09:22 -0800

    create user variable in login

---
+++ WalkWithMe/components/ratings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  Navigator,
+  TouchableOpacity } from 'react-native';
+
+class RatingsForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+}
--- WalkWithMe/index.ios.js
+++ WalkWithMe/index.ios.js
@@ -3,64 +3,64 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import * as firebase from 'firebase';
+ import * as firebase from 'firebase';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  NavigatorIOS
-} from 'react-native';
-import FBSDK from 'react-native-fbsdk';
-import Login from './components/login';
+ import React, { Component } from 'react';
+ import {
+   AppRegistry,
+   StyleSheet,
+   Text,
+   View,
+   NavigatorIOS
+ } from 'react-native';
+ import FBSDK from 'react-native-fbsdk';
+ import Login from './components/login';
 
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDiDp0eq75WK0MAeVAQL3aA9EptcIyWL2U",
-  authDomain: "walk-with-me-6cf40.firebaseapp.com",
-  databaseURL: "https://walk-with-me-6cf40.firebaseio.com",
-  storageBucket: "walk-with-me-6cf40.appspot.com",
-  messagingSenderId: "199658348422"
-};
+ // Initialize Firebase
+ const firebaseConfig = {
+   apiKey: "AIzaSyDiDp0eq75WK0MAeVAQL3aA9EptcIyWL2U",
+   authDomain: "walk-with-me-6cf40.firebaseapp.com",
+   databaseURL: "https://walk-with-me-6cf40.firebaseio.com",
+   storageBucket: "walk-with-me-6cf40.appspot.com",
+   messagingSenderId: "199658348422"
+ };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export default class WalkWithMe extends Component {
+ export default class WalkWithMe extends Component {
 
-  render() {
-    return (
-      <NavigatorIOS
-        initialRoute={{
-          component: Login,
-          title: 'Login'
-        }}
-        navigationBarHidden={true}
-        style={{flex: 1}}
-      />
-    );
-  }
-}
+   render() {
+     return (
+       <NavigatorIOS
+         initialRoute={{
+           component: Login,
+           title: 'Login'
+         }}
+         navigationBarHidden={true}
+         style={{flex: 1}}
+       />
+     );
+   }
+ }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#F5FCFF',
+   },
+   welcome: {
+     fontSize: 20,
+     textAlign: 'center',
+     margin: 10,
+   },
+   instructions: {
+     textAlign: 'center',
+     color: '#333333',
+     marginBottom: 5,
+   },
+ });
 
-AppRegistry.registerComponent('WalkWithMe', () => WalkWithMe);
+ AppRegistry.registerComponent('WalkWithMe', () => WalkWithMe);
