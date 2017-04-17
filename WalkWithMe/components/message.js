cc4f2389140000efc96908d4c326a4d0ae5fdbea
Jeff Gronewold <jeff.gronewold@gmail.com>
2017-04-16 23:29:16 -0500

    progress on messages

--- WalkWithMe/components/message.js
+++ WalkWithMe/components/message.js
@@ -13,7 +13,18 @@ class Message extends React.Component {
   constructor(props) {
     super(props);
 
+    this.messages = firebase.database().ref('messages');
   }
 
-  
+  componentDidMount() {
+    /// do something here to add a listener for new messages
+    // still need to do the followers side of things to set it up to receive
+    // messages
+    // also need to set up the cancel match bits to clear messageKey
+    // in here map through a bunch of views that justify the Text content either
+    // to the left or the right depending on whether the message is from the
+    // author or the follower --> probably need to pass the user through then
+    // as well as props to be able to check and see
+  }
+
 }
